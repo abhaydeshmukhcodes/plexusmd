aa1a12a8a42ea982c5b4a4ac91a2a660fe1bee52
Abhay Deshmukh <deshmukhabhayvilas@gmail.com>
2019-05-20 16:26:43 +0530

    Updates

---
+++ precache-manifest.a5c2d88990022bfaea8c2d04cd8b1a05.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d9b9b2c6cdc65ab9d9413622f43ca8c4",
+    "url": "/new/index.html"
+  },
+  {
+    "revision": "5ec31a349325cb72b6c2",
+    "url": "/new/static/css/2.f465542e.chunk.css"
+  },
+  {
+    "revision": "cdd52b55fdcb8f8211bd",
+    "url": "/new/static/css/main.1779617f.chunk.css"
+  },
+  {
+    "revision": "5ec31a349325cb72b6c2",
+    "url": "/new/static/js/2.e86a0f67.chunk.js"
+  },
+  {
+    "revision": "cdd52b55fdcb8f8211bd",
+    "url": "/new/static/js/main.ec23e87f.chunk.js"
+  },
+  {
+    "revision": "203f4010d004ffdf2b66",
+    "url": "/new/static/js/runtime~main.dee46077.js"
+  },
+  {
+    "revision": "1a38c79bfa93e7b52f448fad6f9a936c",
+    "url": "/new/static/media/banner.1a38c79b.png"
+  },
+  {
+    "revision": "d8e6866effb7404e3c6cfec5d5917845",
+    "url": "/new/static/media/img1.d8e6866e.png"
+  }
+]);
